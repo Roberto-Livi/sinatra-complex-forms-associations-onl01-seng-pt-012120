849bed82d7596b1963c0de1fbfd92e61728fd967
Roberto Livi <robertomlivi@outlook.com>
2020-04-14 02:02:38 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -26,12 +26,18 @@ class PetsController < ApplicationController
   
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
-    
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(params[:owner])
+    end
+    
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
