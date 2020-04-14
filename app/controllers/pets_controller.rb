03fb66016316293e2dd07e099025cb627b29db76
Roberto Livi <robertomlivi@outlook.com>
2020-04-14 02:16:43 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do 
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(params[:owner])
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
@@ -35,7 +35,7 @@ class PetsController < ApplicationController
     @pet.update(params["pet"])
     
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(params[:owner])
     end
     
     @pet.save
